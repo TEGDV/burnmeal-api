7b8d8489b5def8848d80bf9fa666f07ecf27fd64
TEGDV <apjair97@gmail.com>
2021-05-08 15:54:44 -0500

    :clow_face: Testing jwt mock endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from api import views as api_views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("register/", api_views.registration),
-    path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("protected/", api_views.some_protected_view),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
