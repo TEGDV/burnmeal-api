e7da20ac9939fd10e5e1cbb2db2ad32d187fa991
TEGDV <apjair97@gmail.com>
2021-05-08 15:55:15 -0500

    :clow_face: Testing mock auth views

--- api/views.py
+++ api/views.py
@@ -1,22 +1,31 @@
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 from api.serializers import RegistrationSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 @api_view(["POST"])
 def registration(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         serializer = RegistrationSerializer(data=request.data)
     data = dict()
     if serializer.is_valid():
         user = serializer.save()
-        data['response'] = "Succesfully registered new user."
-        data['email'] = user.email
-        data['username'] = user.username
+        data["response"] = "Succesfully registered new user."
+        data["email"] = user.email
+        data["username"] = user.username
     else:
         data = serializer.errors
     return Response(data)
 
-def obtain_auth_token():
-    pass
+
+@api_view(["GET"])
+@authentication_classes([JWTAuthentication])
+def some_protected_view(request):
+    data = {"message": "Si Jalo"}
+    return Response(data)
